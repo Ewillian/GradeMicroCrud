92731c5bb2e97dad35187d847d0812385d892f49
BenoitYnov <43681702+BenoitYnov@users.noreply.github.com>
2020-02-03 11:24:22 +0100

    teacher

---
+++ teacherCore/src/main/java/com/Ewillian/teacherCore/TeacherCoreApplication.java
@@ -0,0 +1,13 @@
+package com.Ewillian.teacherCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherCoreApplication.class, args);
+	}
+
+}
